b0fac5ad849327a7ca847d664f53b6af23aadf64
Vipul Saraswat <95919841+Vwarrior07@users.noreply.github.com>
2025-12-30 02:51:55 +0530

    Prepare project for production deployment

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
